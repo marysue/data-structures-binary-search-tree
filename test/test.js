185296b425188fcf92e384a10598838f098c7d7c
marysue <mary@marylark.com>
2020-09-01 15:26:51 -0600

    Close to finished without Leet

--- test/test.js
+++ test/test.js
@@ -30,96 +30,96 @@ describe('BST', () => {
             expect(tree.root.right.right.val).to.equal(16);
         });
 
-        context('when the BST is empty', () => {
-            it('should correctly insert a TreeNode with the given val as the root', () => {
-                let tree = new BST();
-                tree.insert(10);
-                expect(tree.root).to.be.instanceOf(TreeNode);
-                expect(tree.root.val).to.equal(10);
-            });
-        });
-    });
+    //     context('when the BST is empty', () => {
+    //         it('should correctly insert a TreeNode with the given val as the root', () => {
+    //             let tree = new BST();
+    //             tree.insert(10);
+    //             expect(tree.root).to.be.instanceOf(TreeNode);
+    //             expect(tree.root.val).to.equal(10);
+    //         });
+    //     });
+    // });
 
-    describe('#searchRecur(val)', () => {
-        let tree;
+    // describe('#searchRecur(val)', () => {
+    //     let tree;
 
-        beforeEach(() => {
-            tree = new BST();
-            tree.insert(10);
-            tree.insert(5);
-            tree.insert(16);
-            tree.insert(1);
-            tree.insert(7);
-            tree.insert(16);
-        });
+    //     beforeEach(() => {
+    //         tree = new BST();
+    //         tree.insert(10);
+    //         tree.insert(5);
+    //         tree.insert(16);
+    //         tree.insert(1);
+    //         tree.insert(7);
+    //         tree.insert(16);
+    //     });
 
-        it('should return false if the BST is empty', () => {
-            let emptyTree = new BST();
-            expect(emptyTree.searchRecur(10)).to.equal(false);
-        });
+    //     it('should return false if the BST is empty', () => {
+    //         let emptyTree = new BST();
+    //         expect(emptyTree.searchRecur(10)).to.equal(false);
+    //     });
 
-        it('should be recursive', () => {
-            spy.on(tree, 'searchRecur');
-            tree.searchRecur(7);
-            expect(tree.searchRecur).to.have.been.called.above(1);
-        });
+    //     it('should be recursive', () => {
+    //         spy.on(tree, 'searchRecur');
+    //         tree.searchRecur(7);
+    //         expect(tree.searchRecur).to.have.been.called.above(1);
+    //     });
 
-        context('when the val is contained in the BST', () => {
-            it ('should return true', () => {
-                expect(tree.searchRecur(10)).to.equal(true);
-                expect(tree.searchRecur(7)).to.equal(true);
-                expect(tree.searchRecur(16)).to.equal(true);
-            });
-        });
+    //     context('when the val is contained in the BST', () => {
+    //         it ('should return true', () => {
+    //             expect(tree.searchRecur(10)).to.equal(true);
+    //             expect(tree.searchRecur(7)).to.equal(true);
+    //             expect(tree.searchRecur(16)).to.equal(true);
+    //         });
+    //     });
 
-        context('when the val is not contained in the BST', () => {
-            it ('should return false', () => {
-                expect(tree.searchRecur(-4)).to.equal(false);
-                expect(tree.searchRecur(1.5)).to.equal(false);
-                expect(tree.searchRecur(14)).to.equal(false);
-            });
-        });
-    });
+    //     context('when the val is not contained in the BST', () => {
+    //         it ('should return false', () => {
+    //             expect(tree.searchRecur(-4)).to.equal(false);
+    //             expect(tree.searchRecur(1.5)).to.equal(false);
+    //             expect(tree.searchRecur(14)).to.equal(false);
+    //         });
+    //     });
+    // });
 
-    describe('#searchIter(val)', () => {
-        let tree;
+    // describe('#searchIter(val)', () => {
+    //     let tree;
 
-        beforeEach(() => {
-            tree = new BST();
-            tree.insert(10);
-            tree.insert(5);
-            tree.insert(16);
-            tree.insert(1);
-            tree.insert(7);
-            tree.insert(16);
-        });
+    //     beforeEach(() => {
+    //         tree = new BST();
+    //         tree.insert(10);
+    //         tree.insert(5);
+    //         tree.insert(16);
+    //         tree.insert(1);
+    //         tree.insert(7);
+    //         tree.insert(16);
+    //     });
 
-        it('should return false if the BST is empty', () => {
-            let emptyTree = new BST();
-            expect(emptyTree.searchIter(10)).to.equal(false);
-        });
+    //     it('should return false if the BST is empty', () => {
+    //         let emptyTree = new BST();
+    //         expect(emptyTree.searchIter(10)).to.equal(false);
+    //     });
 
-        it('should be iterative, not recursive', () => {
-            spy.on(tree, 'searchIter');
-            tree.searchIter(7);
-            expect(tree.searchIter).to.have.been.called.once;
-        });
+    //     it('should be iterative, not recursive', () => {
+    //         spy.on(tree, 'searchIter');
+    //         tree.searchIter(7);
+    //         expect(tree.searchIter).to.have.been.called.once;
+    //     });
 
-        context('when the val is contained in the BST', () => {
-            it ('should return true', () => {
-                expect(tree.searchIter(10)).to.equal(true);
-                expect(tree.searchIter(7)).to.equal(true);
-                expect(tree.searchIter(16)).to.equal(true);
-            });
-        });
+    //     context('when the val is contained in the BST', () => {
+    //         it ('should return true', () => {
+    //             expect(tree.searchIter(10)).to.equal(true);
+    //             expect(tree.searchIter(7)).to.equal(true);
+    //             expect(tree.searchIter(16)).to.equal(true);
+    //         });
+    //     });
 
-        context('when the val is not contained in the BST', () => {
-            it ('should return false', () => {
-                expect(tree.searchIter(-4)).to.equal(false);
-                expect(tree.searchIter(1.5)).to.equal(false);
-                expect(tree.searchIter(14)).to.equal(false);
-            });
-        });
+    //     context('when the val is not contained in the BST', () => {
+    //         it ('should return false', () => {
+    //             expect(tree.searchIter(-4)).to.equal(false);
+    //             expect(tree.searchIter(1.5)).to.equal(false);
+    //             expect(tree.searchIter(14)).to.equal(false);
+    //         });
+    //     });
     });
 });
 
